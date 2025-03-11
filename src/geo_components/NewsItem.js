4e28bca1ac155c078c12769d0c513dede34dcdf4
shace-tariq <150523530+shace-tariq@users.noreply.github.com>
2025-03-11 16:43:42 +0500

    Update NewsItem.js

--- src/geo_components/NewsItem.js
+++ src/geo_components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
 <>
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={!imageUrl?"https://ichef.bbci.co.uk/ace/branded_weather/1200/cpsprodpb/b5ad/live/3dff2be0-fb60-11ef-a79f-495dc9a11716.jpg":imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"http://ichef.bbci.co.uk/ace/branded_weather/1200/cpsprodpb/b5ad/live/3dff2be0-fb60-11ef-a79f-495dc9a11716.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">  {title} ...</h5>
             <p className="card-text">{description}...</p>
